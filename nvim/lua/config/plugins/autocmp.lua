1fb5f7cd021f5c2ac324e0ba72a30921da1fd8fe
thedwncmpny-mini <mail@edwinolivaers.com>
2026-05-11 22:34:29 -0700

    repair snippet functionality

--- nvim/lua/config/plugins/autocmp.lua
+++ nvim/lua/config/plugins/autocmp.lua
@@ -9,35 +9,49 @@ return {
     "rafamadriz/friendly-snippets", -- useful snippets
   },
   config = function()
-    local cmp = require("cmp")
+    local cmp = require "cmp"
 
-    local luasnip = require("luasnip")
+    local luasnip = require "luasnip"
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
 
-    cmp.setup({
+    cmp.setup {
       snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
+        expand = function(args) luasnip.lsp_expand(args.body) end,
       },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+      mapping = cmp.mapping.preset.insert {
+        ["<C-k>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<C-j>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      }),
+        ["<CR>"] = cmp.mapping.confirm { select = false },
+      },
       -- sources for autocompletion
-      sources = cmp.config.sources({
+      sources = cmp.config.sources {
         { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
-      }),
-    })
+      },
+    }
   end,
 }
